ba960bc990efab9610a4c67f801777aa0b2b9d62
ShakibRamez <shakibramiz1@icloud.com>
2025-06-24 11:04:05 +0430

    laravel instaled and routs and views worked

---
+++ resources/views/new/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>page 2</title>
+</head>
+<body>
+    <h1>index page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta enim fuga beatae. Sapiente placeat nisi molestias iste non possimus neque ipsa molestiae officiis pariatur? Dignissimos excepturi maiores eum illum dolorem.</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+       <h1>Laravel staet</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ab vero odit harum perspiciatis? Debitis commodi voluptate iusto consequuntur, repellat laudantium explicabo laborum quas blanditiis nemo repudiandae maxime perspiciatis nostrum!</p>
+
+
+       <a href="./new">click me to go index file</a>
+    </body>
+</html>
